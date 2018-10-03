b2189d19132c46d481561f9179fcff035403e153
Brian Hunley <brian.hunley@gmail.com>
2018-10-03 08:18:44 -0400

    added more examples

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const moment = require('moment');
+const path = require('path');
 const zip = require('express-zip');
+const zipEasy = require('express-easy-zip');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send(
-  '<h3>Welcome to the Express Zipfile Download Example Code!</h3>' +
-  '<a href="/getzip">Download Zip</a>'
+  '<h3>NPM "express-zip" and "express-easy-zip" Example Code</h3>' +
+  '<p><a href="/getzip">Zip and Download Using Express-Zip</a></p> ' +
+  '<p><a href="/getzip2">Zip and Download Using Express-Easy-Zip</a></p>'
 ));
 
 app.get('/getzip', (req, res) => {
@@ -22,6 +26,38 @@ app.get('/getzip', (req, res) => {
       console.log(`Zipfile download complete!  The zipfile size is ${bytesZipped}.`)
     };
   });
+});
+
+app.use(zipEasy());
+app.get('/getzip2', (req, res) => {
+    // console.log('router post zip');
+    console.log(req.body);
+
+    //res.json({
+    //   msg: "Yep, data receipt confirmed"
+    //});
+
+    // const downloads = req.body.downloads;
+
+    const currDateTime = moment().format('YYYY-MM-DD_HH-mm-ss');
+    console.log(currDateTime);
+
+    res.zip({
+      files: [
+        { path: path.join(__dirname, './files/'), name: '/' },
+        { path: path.join(__dirname, './images/'), name: '/' }
+      ],
+      filename: `sulimo-logs-${currDateTime}.zip`
+    })
+    .then(obj => {
+      const zipFileSizeInBytes = obj.size;
+      const ignoredFileArray = obj.ignored;
+
+      console.log(`Zipfile download complete!  The zipfile size is ${zipFileSizeInBytes}.`);
+    })
+    .catch((err) => {
+      console.log(err); // if zip fails
+    });
 });
 
 app.listen(port, () => console.log(`Express Server listening on port ${port}...`));
